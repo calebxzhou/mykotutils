ba62f6c123e6287ee23dda601c1453cebc71b4dc
calebxzhou <me@calebxzhou.cn>
2025-12-20 22:22:52 +0800

    customize date time pattern

--- std/src/main/kotlin/calebxzhou/mykotutils/std/DateUtils.kt
+++ std/src/main/kotlin/calebxzhou/mykotutils/std/DateUtils.kt
@@ -1,12 +1,19 @@
 package calebxzhou.mykotutils.std
 
+import java.time.Instant
 import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 /**
  * calebxzhou @ 2025-12-19 19:38
  */
-val dateTimeNow
-    get() = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"))
+const val DEFAULT_DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss"
+private fun getDateTimeFormatter(pattern: String = DEFAULT_DATE_TIME_PATTERN) = DateTimeFormatter.ofPattern(pattern)
+fun getDateTimeNow(pattern: String = DEFAULT_DATE_TIME_PATTERN) = LocalDateTime.now().format(getDateTimeFormatter(pattern))
 val humanDateTimeNow
-    get() = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+    get() = getDateTimeNow(DEFAULT_DATE_TIME_PATTERN)
+val Int.secondsToHumanDateTime: String
+    get() = Instant.ofEpochSecond(this.toLong()).atZone(ZoneId.systemDefault()).format(getDateTimeFormatter(DEFAULT_DATE_TIME_PATTERN))
+val Long.millisToHumanDateTime: String
+    get() = Instant.ofEpochMilli(this).atZone(ZoneId.systemDefault()).format(getDateTimeFormatter(DEFAULT_DATE_TIME_PATTERN))
