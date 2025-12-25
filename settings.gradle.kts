68c3281a76b5b05f1deccc6b9f4d99c6bcdeda95
calebxzhou <me@calebxzhou.cn>
2025-12-25 11:37:53 +0800

    +projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,11 @@ dependencyResolutionManagement {
 }
 
 
-include(":std", ":log", ":ktor")
+include(":std", ":log", ":ktor",":curseforge","modrinth","hwspec","mojang")
 project(":std").projectDir = file("std")
 project(":log").projectDir = file("log")
 project(":ktor").projectDir = file("ktor")
+project(":curseforge").projectDir = file("curseforge")
+project(":modrinth").projectDir = file("modrinth")
+project(":hwspec").projectDir = file("hwspec")
+project(":mojang").projectDir = file("mojang")
