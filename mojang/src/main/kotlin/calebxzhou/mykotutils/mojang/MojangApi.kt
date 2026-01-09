ff181681d01c02a751056b30eac9bfea5446293d
calebxzhou <me@calebxzhou.cn>
2026-01-09 23:28:46 +0800

    http client with content nego

--- mojang/src/main/kotlin/calebxzhou/mykotutils/mojang/MojangApi.kt
+++ mojang/src/main/kotlin/calebxzhou/mykotutils/mojang/MojangApi.kt
@@ -5,17 +5,27 @@ import calebxzhou.mykotutils.std.Ok
 import calebxzhou.mykotutils.std.decodeBase64
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.request
 import io.ktor.client.request.url
 import io.ktor.http.isSuccess
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
 object MojangApi {
     private val lgr by Loggers
+    private val httpClient get() = HttpClient {
+        install(ContentNegotiation){
+            json(Json {
+                ignoreUnknownKeys = true
+            })
+        }
+    }
     suspend fun getUuidFromName(name: String): Result<String?> = runCatching {
-        HttpClient().use {
+        httpClient.use {
             val resp = it.request { url("https://api.mojang.com/users/profiles/minecraft/${name}") }
-
+            @Serializable
             data class IdName(val name: String, val id: String)
             if(resp.status.value==404) return Ok(null)
             val body = resp.body<IdName>()
@@ -24,7 +34,7 @@ object MojangApi {
     }
 
     suspend fun getProfile(uuidNoDash: String): Result<MojangProfileResponse> = runCatching {
-        HttpClient().use {
+        httpClient.use {
             val resp = it.request { url("https://sessionserver.mojang.com/session/minecraft/profile/$uuidNoDash") }
             val profile = resp.body<MojangProfileResponse>()
             profile
