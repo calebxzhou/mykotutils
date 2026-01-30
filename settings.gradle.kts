430e800cdca7d8e7549a0b730855947e770783f1
calebxzhou <me@calebxzhou.cn>
2026-01-31 00:01:15 +0800

    no need, merged back to rdi

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,11 +15,9 @@ dependencyResolutionManagement {
 }
 
 
-include(":std", ":log", ":ktor",":curseforge","modrinth","hwspec","mojang")
+include(":std", ":log", ":ktor", "hwspec","mojang")
 project(":std").projectDir = file("std")
 project(":log").projectDir = file("log")
 project(":ktor").projectDir = file("ktor")
-project(":curseforge").projectDir = file("curseforge")
-project(":modrinth").projectDir = file("modrinth")
 project(":hwspec").projectDir = file("hwspec")
 project(":mojang").projectDir = file("mojang")
