4348531c1ed034f4048a6a796641fe1ab1309a16
calebxzhou <me@calebxzhou.cn>
2026-02-09 22:47:05 +0800

    move back to rdi

--- std/src/main/kotlin/calebxzhou/mykotutils/std/Utils.kt
+++ std/src/main/kotlin/calebxzhou/mykotutils/std/Utils.kt
@@ -6,14 +6,4 @@ import kotlinx.coroutines.Dispatchers
 /**
  * calebxzhou @ 2026-01-09 22:25
  */
-inline fun <reified T> Ok(obj: T): Result<T> {
-    return Result.success(obj)
-}
-fun Ok(): Result<Unit> {
-    return Result.success(Unit)
-}
-
-inline fun <reified T> Err(obj: Throwable): Result<T> {
-    return Result.failure(obj)
-}
 val ioScope get() = CoroutineScope(Dispatchers.IO)
